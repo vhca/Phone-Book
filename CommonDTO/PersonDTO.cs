ff766675ba7bbaf8aee10b2118eec95fb575202e
Jose Cespedes <jhosep50@gmail.com>
2019-06-07 17:28:21 -0400

    create the Structure of the project for Phone Book

---
+++ CommonDTO/PersonDTO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace CommonDTO
+{
+    [DataContract]
+    public class PersonDTO
+    {
+        #region Properties
+
+        [DataMember]
+        [Key]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        [Required]
+        [MaxLength(15)]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string MiddleName { get; set; }
+
+        [DataMember]
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string Gender { get; set; }
+
+        [DataMember]
+        public int Age { get; set; }
+
+        [DataMember]
+        public string ImageSource { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public PersonDTO()
+        {
+            Id = Guid.NewGuid();
+        }
+        #endregion        
+    }
+}
---
+++ PhoneBook/App_Start/WebApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Web.Http;
+using CommonDTO;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.OData;
+using Microsoft.OData.Edm;
+
+namespace PhoneBook
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+            config.MapODataServiceRoute("ODataRoute", "odata", GetEdmModel());
+            config.EnsureInitialized();
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder
+            {
+                Namespace = "PhoneBook",
+                ContainerName = "PhoneBookContainer"
+            };
+
+            builder.EntitySet<PersonDTO>("Person");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
